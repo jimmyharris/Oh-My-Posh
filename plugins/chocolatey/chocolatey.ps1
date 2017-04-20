139bd447bc39923b259d032ca5b7e1c6f57df408
James Harris <james.harris@garmin.com>
2017-04-20 13:29:05 -0500

    Add plugin to auto load chocolatey
    This patch adds a plugin to automatically load chocoately's helper
    modules if chocolatey is installed.

---
+++ plugins/chocolatey/chocolatey.ps1
@@ -0,0 +1,7 @@
+# Install the chocolatey profile helpers in Powershell if chocolatey is
+# installed.
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
